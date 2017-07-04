0daeb31e0bbee695c4c64e8e7e1d937b97b0c7c4
ltp <1776494277@qq.com>
2017-07-04 21:50:46 +0800

    Initial commit

---
+++ menucanvas.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2017/5/25.
+ */
+
+var can=document.getElementById("menu");
+var ctx2=can.getContext('2d');
+
+/*
+ctx2.fillStyle="yellow";
+ctx2.fillRect(0,0,700,50);
+
+ctx2.beginPath();
+ctx2.moveTo(0,0);
+ctx2.lineTo(30,25);
+ctx2.lineTo(0,50);
+ctx2.stroke();*/
